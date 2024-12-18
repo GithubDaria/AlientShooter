df99fedfe4436040d6027f07af7f07e1d3df9cb5
GithubDaria <162898759+GithubDaria@users.noreply.github.com>
2024-12-18 03:11:46 +0200

    Update files

--- Assets/Scripts/UI/ChangeButtonType.cs
+++ Assets/Scripts/UI/ChangeButtonType.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public class ChangeButtonType : MonoBehaviour
 {
     [SerializeField] private PlayerGun PlayerGun;
-    [SerializeField] private GameObject regularBulletPrefab; // Prefab for the regular bullet
-    [SerializeField] private GameObject explosiveBulletPrefab; // Prefab for the explosive bullet
-    [SerializeField] private GameObject ricochetBulletPrefab; // Prefab for the ricochet bullet
+    [SerializeField] private GameObject BulletTypePrefab;
 
-    public void SelectRegularBullet()
-    {
-        PlayerGun.SetBulletType(regularBulletPrefab); 
-    }
 
-    public void SelectExplosiveBullet()
+    public void SelectBullet()
     {
-        PlayerGun.SetBulletType(explosiveBulletPrefab); 
-    }
-
-   public void SelectRicochetBullet()
-    {
-        PlayerGun.SetBulletType(ricochetBulletPrefab); 
+        PlayerGun.SetBulletType(BulletTypePrefab); 
     }
 }
